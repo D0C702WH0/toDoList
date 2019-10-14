1f66bcce21f04bbbabf7c018cee832d45a32207b
D0C702WH0 <jemyplu@gmail.com>
2019-10-15 01:56:45 +0200

    element can be added to the list

--- script/toDoList.js
+++ script/toDoList.js
@@ -1,6 +1,12 @@
-function getInputValue() {
+// function getInputValue() {
+//     const toto = document.getElementById("inputItem").value;
+//     console.log(toto);  
+// }
 
-    const toto = document.getElementById("inputItem").value;
-    console.log(toto);
-    
+function addItemToList() {
+    const input = document.getElementById("inputItem").value;
+    const ul = document.getElementById("ItemsList");
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(input));
+    ul.appendChild(li);  
 }
