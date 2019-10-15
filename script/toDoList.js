2448193a5201bfa266ec270c4def8bde40275c62
D0C702WH0 <jemyplu@gmail.com>
2019-10-15 03:13:01 +0200

    element can be marked as done

--- script/toDoList.js
+++ script/toDoList.js
@@ -1,6 +1,14 @@
-function getInputValue() {
+const list = document.querySelector('ul');
+list.addEventListener('click', (ev) => {
+const element = document.getElementById(`${ev.target.id}`);
+element.classList.add("done");
+}, false);
 
-    const toto = document.getElementById("inputItem").value;
-    console.log(toto);
-    
+const addItemToList = () => {
+    const input = document.getElementById("inputItem").value;
+    const ul = document.getElementById("ItemsList");
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(input));
+    li.setAttribute('id',`${input}`);
+    ul.appendChild(li);  
 }
